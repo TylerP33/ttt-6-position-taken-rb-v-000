46fde7b5ff716187b167b4e9f3fd05b9c1025e19
Tyler Pulse <tyler3321@gmail.com>
2017-10-10 16:30:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board, index)
   if "#{board[0]} == "  " || """
      false
-  elsif "#{board[1]}" == "X"
-    returns true
+  elsif "#{board[1]}" == "X" || "O"
+     true
 
   else
     end
